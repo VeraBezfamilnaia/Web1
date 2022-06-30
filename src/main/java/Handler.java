ee0653b334ae2163d081e30e9041b2cd4d598fdf
VeraLizunova <vera.lizunova.98@mail.ru>
2022-06-30 16:54:32 +0300

    added Handler

---
+++ src/main/java/Handler.java
@@ -0,0 +1,92 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Handler {
+    private static final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
+            "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    public static void handleRequest(Socket socket) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream())
+        ) {
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+            checkRequestParams(parts, socket);
+
+            final var path = parts[1];
+            handleNotFoundCase(out, path, socket);
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+            handleClassicCase(path, filePath, out, mimeType, socket);
+
+            final var length = Files.size(filePath);
+            handleSuccessfulCase(filePath, length, out, mimeType, socket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkRequestParams(String[] parts, Socket socket) throws IOException {
+        if (parts.length != 3) {
+            socket.close();
+        }
+    }
+
+    private static void handleNotFoundCase(BufferedOutputStream out, String path, Socket socket) throws IOException {
+        if (!validPaths.contains(path)) {
+            out.write((
+                    "HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Length: 0\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.flush();
+            socket.close();
+        }
+    }
+
+    private static void handleClassicCase(String path, Path filePath, BufferedOutputStream out, String mimeType
+            , Socket socket) throws IOException {
+        if (path.equals("/classic.html")) {
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+            socket.close();
+        }
+    }
+
+    private static void handleSuccessfulCase(Path filePath, long length, BufferedOutputStream out, String mimeType,
+                                             Socket socket)
+            throws IOException {
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
+        socket.close();
+    }
+}
