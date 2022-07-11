c5f7ac8b7e899989056457c4b551f6a4b1d76bd7
VeraLizunova <vera.lizunova.98@mail.ru>
2022-07-11 15:16:45 +0300

    fixed Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,6 @@
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            var serverSocket = Server.start(9999);
-            while (true) {
-                var socket = serverSocket.accept();
-                Server.handle(socket);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        var server = new Server();
+        server.listen(9999);
     }
 }
