067bcedd2ac51c2300fe0481d17ea5a9ef541bd5
VeraLizunova <vera.lizunova.98@mail.ru>
2022-07-12 19:53:32 +0300

    fixed Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,37 @@
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
+        var server = new Server();
+        server.addHandler("GET", "/messages", Main::handleImpl);
+        server.addHandler("POST", "/messages", Main::handleImpl);
+        server.listen(9999);
+    }
+
+    private static void handleImpl(Request request, BufferedOutputStream responseStream) {
+        final var filePath = Path.of(".", "public", request.getPath());
         try {
-            var serverSocket = Server.start(9999);
-            while (true) {
-                var socket = serverSocket.accept();
-                Server.handle(socket);
-            }
+            final String mimeType = Files.probeContentType(filePath);
+            final var length = Files.size(filePath);
+            handleSuccessfulCase(filePath, length, responseStream, mimeType);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void handleSuccessfulCase(Path filePath, long length, BufferedOutputStream out, String mimeType)
+            throws IOException {
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
     }
 }
